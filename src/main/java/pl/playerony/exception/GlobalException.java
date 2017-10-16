87141813bdd3dedadaa64dfb88138e6570cd011b
Paweł <pawel.wojtasinski.1995@gmail.com>
2017-10-16 22:55:50 +0200

    Create GlobalException class

---
+++ src/main/java/pl/playerony/exception/GlobalException.java
@@ -0,0 +1,30 @@
+package pl.playerony.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalException {
+	@ExceptionHandler(DatabaseException.class)
+	public Map<String, Object> handleDatabaseException(DatabaseException e) {
+		Map<String, Object> map = new HashMap<>();
+		
+		map.put("status", "Error[DatabaseException]: " + e.getMessage());
+		e.printStackTrace();
+		
+		return map;
+	}
+	
+	@ExceptionHandler(InputException.class)
+	public Map<String, Object> handleInputException(InputException e) {
+		Map<String, Object> map = new HashMap<>();
+		
+		map.put("status", "Error[InputException]: " + e.getMessage());
+		e.printStackTrace();
+		
+		return map;
+	}
+}
