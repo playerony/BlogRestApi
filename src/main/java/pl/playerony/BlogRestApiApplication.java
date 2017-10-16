76bde9eb3c6249be8f013561356bea28e3fa4784
Paweł <pawel.wojtasinski.1995@gmail.com>
2017-10-16 20:01:28 +0200

    Initial commit

---
+++ src/main/java/pl/playerony/BlogRestApiApplication.java
@@ -0,0 +1,12 @@
+package pl.playerony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogRestApiApplication.class, args);
+	}
+}
