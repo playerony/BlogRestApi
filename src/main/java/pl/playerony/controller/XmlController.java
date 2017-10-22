047d308f8b27a7d5473f06abc24c723cd645669c
Paweł <pawel.wojtasinski.1995@gmail.com>
2017-10-22 17:43:27 +0200

    Create 0Auth2 controller

---
+++ src/main/java/pl/playerony/controller/XmlController.java
@@ -0,0 +1,12 @@
+package pl.playerony.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin
+@RequestMapping("/xml")
+public class XmlController {
+	// TODO
+}
